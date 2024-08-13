3b6c872a000278bec3167863eb2f64e0e226bf90
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2024-08-13 12:49:34 +0900

    update __main__.py

---
+++ fasp/__main__.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+from fasp import fasta
+from fasp import fastp
+from fasp import fastn
+
+def main():
+    args = parse_args()
+    function = functions[args.function]
+    try:
+        function(*args.args)
+    except TypeError as e:
+        print(e)
+        sys.exit(1)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('function', choices=functions)
+    parser.add_argument('args', type=str, nargs='*')
+    args = parser.parse_args()
+    return args
+
+functions = {}
+
+if __name__ == "__main__":
+    main()
